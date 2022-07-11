6ee47824c7799ad6c3180f108d91111d0da5d787
MacbookAir <florent2303@pm.me>
2022-07-11 17:47:26 +0200

    react router

--- src/modules/GameArea/Gamearea.tsx
+++ src/modules/GameArea/Gamearea.tsx
@@ -1,3 +1,26 @@
+import './GameArea.css'
+
 export default function Gamearea() {
-  return <></>
+  return (
+    <main>
+      <h1>Memory</h1>
+      <section className="board">
+        <article className="board-case">
+          <div className="board-case-content">1</div>
+        </article>
+        <article className="board-case">
+          <div className="board-case-content">2</div>
+        </article>
+        <article className="board-case">
+          <div className="board-case-content">3</div>
+        </article>
+        <article className="board-case">
+          <div className="board-case-content">4</div>
+        </article>
+        <article className="board-case">
+          <div className="board-case-content">5</div>
+        </article>
+      </section>
+    </main>
+  )
 }
---
+++ src/modules/GameConfig/Gameconfig.tsx
@@ -0,0 +1,7 @@
+export default function Gameconfig() {
+  return (
+    <>
+      <h1>Configuration</h1>
+    </>
+  )
+}
