485fad232a51787751c3b29a89b2a3dbd738fc3a
MacbookAir <florent2303@pm.me>
2022-07-12 08:58:00 +0200

    images

--- src/modules/GameArea/Gamearea.tsx
+++ src/modules/GameArea/Gamearea.tsx
@@ -6,7 +6,13 @@ export default function Gamearea() {
       <h1>Memory</h1>
       <section className="board">
         <article className="board-case">
-          <div className="board-case-content">1</div>
+          <div className="board-case-content">
+            <img
+              src="./images/animals/a-kangaroo-looks-straight-at-the-camera.jpg"
+              alt=""
+              className="board-case-image"
+            />
+          </div>
         </article>
         <article className="board-case">
           <div className="board-case-content">2</div>
